430514fecd89cf02f2fe6076a6e71b7f0ab1bb95
Daniel McCloy <dan.mccloy@gmail.com>
2015-07-17 14:46:08 -0700

    better comments, use pdftk to keep only first page

--- pubs/typeset-abstract.bash
+++ pubs/typeset-abstract.bash
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+## create TeX file
 pandoc -s --latex-engine=xelatex --natbib --no-tex-ligatures --template=template-abstract-anonymous.tex -o LSA-abstract.tex LSA-abstract.md
 
+## run xelatex, bibtex8, xelatex, xelatex, xelatex
 . create.sh LSA-abstract.tex
+
+## delete second page (references)
+pdftk LSA-abstract.pdf cat 1 output LSA-abs-final.pdf
